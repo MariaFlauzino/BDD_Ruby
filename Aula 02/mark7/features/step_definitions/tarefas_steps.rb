1d32301f819078fa58632ff149b675c2c62d5e37
Maria Flauzino <maria.flauzino@fit-tecnologia.org.br>
2018-07-02 14:19:09 -0300

    Aula 3

--- Aula 02/mark7/features/step_definitions/tarefas_steps.rb
+++ Aula 02/mark7/features/step_definitions/tarefas_steps.rb
@@ -1,33 +1,73 @@
-Dado("que eu tenho uma tarefa com os seguintes atributos:") do |table|
-    @tarefas_page = TarefasPage.new
-    @nova_tarefa =  table.rows_hash
+Dado('que eu tenho uma tarefa com os seguintes atributos:') do |table|
+  @nova_tarefa =  table.rows_hash
+  DAO.new.remover_tarefas(@nova_tarefa[:nome])
 end
 
- Dado("eu quero taguear essa tarefa com") do |tags|
-    @tags =  tags.hashes
-  end
+Dado('eu quero taguear essa tarefa com') do |tags|
+  @tags = tags.hashes
+end
 
-  Quando("faço o cadastro dessa tarefa") do
-    visit '/tasks'
-    @tarefas_page.botao_novo.click
-    @tarefas_page.adicionar.cadastrar(@nova_tarefa)
- end
-  
-  Então("devo ver essa tarefa com o status {string}") do |status_tarefa|
-    tarefa = @tarefas_page.item(@nova_tarefa[:nome])
-    expect(tarefa).to have_content status_tarefa
-  end
-  
-  Dado("eu já tinha cadastrado essa tarefa e não tinha percebido") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+Dado('eu já tinha cadastrado essa tarefa e não tinha percebido') do
+  steps %(
+    Quando faço o cadastro dessa tarefa
+  )
+end
 
- 
-  
-  Então("devo ver uma mensagem {string} Ao tentar cadastrar.") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Então("devo ver somente uma tarefa com o nome cadastrado.") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+Quando('faço o cadastro dessa tarefa') do
+  visit '/tasks'
+  @tarefas_page.botao_novo.click
+  @tarefas_page.adicionar.cadastrar(@nova_tarefa, @tags)
+end
+
+Então('devo ver essa tarefa com o status {string}') do |status_tarefa|
+  tarefa = @tarefas_page.item(@nova_tarefa[:nome])
+  expect(tarefa).to have_content status_tarefa
+end
+
+Então('devo ver somente {int} tarefa com o nome cadastrado.') do |quantidade|
+  visit '/tasks'
+  # busca pela tarefa que foi cadastrada, o resultado deve ser igual a 1
+  @tarefas_page.busca(@nova_tarefa[:nome])
+  expect(@tarefas_page.itens.size).to eql quantidade
+end
+
+Então('devo ver a mensagem {string} ao tentar cadastrar.') do |mensagem|
+  expect(@tarefas_page.adicionar.alerta.text).to eql mensagem
+end
+
+
+# remover
+Dado("que eu tenho uma tarefa indesejada") do
+  @nova_tarefa = {nome: 'Tarefa muito boa para ser removida', data: '01/07/2018'}
+  @tags = [{tag: 'remover'}]
+  DAO.new.remover_tarefas(@nova_tarefa[:nome])
+
+  steps %(
+    Quando faço o cadastro dessa tarefa
+  )
+end
+
+Quando("eu solicito a exlusão dessa tarefa") do
+  tarefa_para_remover = @tarefas_page.item(@nova_tarefa[:nome])
+  tarefa_para_remover.find('#delete-button').click
+end
+
+Quando("confirmo a ação de exclusão") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("esta tarefa não deve ser exibida na lista") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu solicito a exclusão dessa tarefa") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu cancelo esta ação") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("esta tarefa permanece na lista") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
--- Aula 02/mark7/features/support/pages/tarefas_page.rb
+++ Aula 02/mark7/features/support/pages/tarefas_page.rb
@@ -1,27 +1,48 @@
 # - Adicionar é classe que representa a subpagina Adicionar tarefas
 # que será tratada como uma Seção do SitePrism
 class Adicionar < SitePrism::Section
-    element :campo_nome, 'input[name=title]'
-    element :campo_data, 'input[name=dueDate]'
-    element :botao_cadastrar, 'button[id*=submit]'
+  element :campo_nome, 'input[name=title]'
+  element :campo_data, 'input[name=dueDate]'
+  element :campo_tags, '.bootstrap-tagsinput input'
+  element :botao_cadastrar, 'button[id*=submit]'
+  element :alerta, '.alert-warn'
 
 
-    def cadastrar(tarefa)
-        campo_nome.set tarefa[:nome]
-        campo_data.set tarefa[:data]
-        botao_cadastrar.click
+  def add_tags(tags)
+    if tags != nil
+      tags.each do |t|
+        campo_tags.set t[:tag]
+        campo_tags.send_keys :tab
+      end
     end
+  end
+
+  def cadastrar(tarefa, tags)
+    campo_nome.set tarefa[:nome]
+    campo_data.set tarefa[:data]
+    add_tags(tags) 
+    botao_cadastrar.click
+  end
 end
 
-
-# - TarefasPage é a pagina principal
+# - TarefasPage eh a pagina principal
 class TarefasPage < SitePrism::Page
-    element :painel, '#task-board'
-    element :botao_novo, '#insert-button'
+  element :painel, '#task-board'
+  element :botao_novo, '#insert-button'
 
-    def item(valor)
-        find('table tbody tr', text: valor)
-    end
+  element :campo_busca, 'input[name=search]'
+  element :botao_busca, '#search-button'
 
-    section :adicionar, Adicionar, '#add-task-view'
+  elements :itens, 'table tbody tr'
+
+  def busca(valor)
+    campo_busca.set valor
+    botao_busca.click
+  end
+  
+  def item(valor)
+    find('table tbody tr', text: valor)
+  end
+
+  section :adicionar, Adicionar, '#add-task-view'
 end
